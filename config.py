39618a1da903982e220c3d70179e2b0991968056
Santoyo18 <santoyoalejandro262@gmail.com>
2024-02-19 07:47:47 -0500

    fg

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 class Config:
-    #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@127.0.0.1:3306/proyecto_py"
-    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:BaA3FehB34CEdfg41Bhe6a15531CaFc3@viaduct.proxy.rlwy.net:35066/proyecto_py"
+    SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:@127.0.0.1:3306/proyecto_py"
+    #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:BaA3FehB34CEdfg41Bhe6a15531CaFc3@viaduct.proxy.rlwy.net:35066/proyecto_py"
     #SQLALCHEMY_DATABASE_URI = "mysql+pymysql://sql10683866:SMdbq2hxIB@sql10.freemysqlhosting.net:3306/sql10683866"
     # mysql://root:hCdBGEd4cCAd3CbgFggGghAefgE6f41-@roundhouse.proxy.rlwy.net:45538/railway
     SQLALCHEMY_TRACK_MODIFICATIONS = True
